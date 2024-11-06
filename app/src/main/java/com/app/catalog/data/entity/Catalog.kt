75936d5ced16f3c9f368894c4447d71513da3953
Abhay Giri <giriabhay47@gmail.com>
2024-11-06 12:38:02 +0530

    Initial Commit

--- app/src/main/java/com/app/catalog/data/entity/Catalog.kt
+++
@@ -1,5 +0,0 @@
-package com.app.catalog.data.entity
-
-import androidx.annotation.DrawableRes
-
-data class Catalog(@DrawableRes val imageRes: Int, val type: CatalogType)
--- app/src/main/java/com/app/catalog/data/entity/CatalogType.kt
+++
@@ -1,9 +0,0 @@
-package com.app.catalog.data.entity
-
-enum class CatalogType {
-    CITY,
-    DOG,
-    FLOWER,
-    CAR,
-    BIKE
-}
