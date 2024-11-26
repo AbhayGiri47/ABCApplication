8e9ee5c5c5e057ae54244e13425179f0e6a5ad92
Abhay Giri <giriabhay47@gmail.com>
2024-11-26 15:53:47 +0530

    Initial Commit

--- app/src/main/java/com/app/carousel/domain/usecase/GetCarouselImageUseCase.kt
+++ app/src/main/java/com/app/carousel/domain/usecase/GetCarouselImageUseCase.kt
@@ -1,10 +1,7 @@
 package com.app.carousel.domain.usecase
 
-import com.app.carousel.base.BaseUseCase
 import com.app.carousel.data.model.Carousel
 import com.app.carousel.domain.respository.CarouselRepository
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
